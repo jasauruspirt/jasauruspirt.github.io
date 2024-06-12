5130d6a93e9f885826eaa7e4d42bd244827d9127
Jasa Pengurusan PIRT - Pangan Industri Rumah Tangga <128014969+jasauruspirt@users.noreply.github.com>
2024-06-12 22:03:29 +0700

    Add files via upload

--- kode/index.js
+++ kode/index.js
@@ -1,27 +1,28 @@
 // *************** Artikel Section ***************
-async function fetchLatestArticles() {
-    try {
-        const response = await fetch('/artikel.json');
-        const data = await response.json();
-        const latestArticles = data.daftar_artikel.slice(-5).reverse(); // Mengambil 5 artikel terbaru dan membalik urutannya
-        displayLatestArticles(latestArticles);
-    } catch (error) {
-        console.error('Terjadi kesalahan saat mengambil artikel:', error);
-    }
-}
+// async function fetchLatestArticles() {
+//     try {
+//         const response = await fetch('/artikel.json');
+//         const data = await response.json();
+//         // Mengambil 5 artikel terbaru dan membalik urutannya
+//         const latestArticles = data.daftar_artikel.slice(-5).reverse();
+//         displayLatestArticles(latestArticles);
+//     } catch (error) {
+//         console.error('Terjadi kesalahan saat mengambil artikel:', error);
+//     }
+// }
 
-function displayLatestArticles(articles) {
-    const boxSliderArtikel = document.querySelector('.box-slider-artikel');
-    boxSliderArtikel.innerHTML = articles.map(article => `
-        <div class="content-slider-artikel swiper-slide slide">
-            <img src="${article.alamat_gambar}" alt="${article.judul}" title="${article.judul}">
-            <h3>${article.judul}</h3>
-            <p>${article.deskripsi}</p>
-        </div>
-    `).join('');
-}
+// function displayLatestArticles(articles) {
+//     const boxSliderArtikel = document.querySelector('.box-slider-artikel');
+//     boxSliderArtikel.innerHTML = articles.map(article => `
+//         <div class="content-slider-artikel swiper-slide slide">
+//             <img src="${article.alamat_gambar}" alt="${article.judul}" title="${article.judul}">
+//             <h3>${article.judul}</h3>
+//             <p>${article.deskripsi}</p>
+//         </div>
+//     `).join('');
+// }
 
-fetchLatestArticles();
+// fetchLatestArticles();
 
 // *************** galeri klien ***************
 const klienSlider = new Swiper(".klien-slider", {
@@ -45,22 +46,22 @@ const klienSlider = new Swiper(".klien-slider", {
 });
 
 // *************** artikel section ***************
-const artikelTerbaruSlider = new Swiper(".container-slider", {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 16,
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        500: {
-            slidesPerView: 2,
-        },
-        800: {
-            slidesPerView: 3,
-        },
-    },
-    autoplay: {
-        delay: 2500,
-    },
-});
+// const artikelTerbaruSlider = new Swiper(".container-slider", {
+//     loop: true,
+//     grabCursor: true,
+//     spaceBetween: 16,
+//     breakpoints: {
+//         0: {
+//             slidesPerView: 1,
+//         },
+//         500: {
+//             slidesPerView: 2,
+//         },
+//         800: {
+//             slidesPerView: 3,
+//         },
+//     },
+//     autoplay: {
+//         delay: 2500,
+//     },
+// });
